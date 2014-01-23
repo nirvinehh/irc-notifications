04bd106e1861e0d0158f803adea36a3cc266b46e
Nick Irvine <nirvine@hotheadgames.com>
2014-01-22 16:32:07 -0800

    Make SETUP.sh clobberful

--- SETUP.sh
+++ SETUP.sh
@@ -8,5 +8,5 @@ if [ ! -d $HOME/.irssi/scripts/autorun ]; then
     mkdir -vp $HOME/.irssi/scripts/autorun;
 fi
 
-ln -s $PWD/irc_appnotify.pl $HOME/.irssi/scripts;
-ln -s $PWD/irc_appnotify.pl $HOME/.irssi/scripts/autorun;
+ln -sf $PWD/irc_appnotify.pl $HOME/.irssi/scripts;
+ln -sf $PWD/irc_appnotify.pl $HOME/.irssi/scripts/autorun;
